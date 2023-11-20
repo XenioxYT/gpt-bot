8819ac2c83614386b4e1df6002bb799e6155a406
Xeniox <tomlockwood135@outlook.com>
2023-11-20 10:21:26 +0000

    Add format_message function to utils

---
+++ utils/format_message.py
@@ -0,0 +1,18 @@
+from utils.get_and_set_timezone import get_timezone
+import datetime
+import pytz
+
+def format_message(discord_id, username, content):
+
+    # Use the timezone associated with the user's Discord ID
+    user_timezone = get_timezone(discord_id)
+    timezone = pytz.timezone(user_timezone)
+    
+    # Get current time in the specified timezone
+    current_time = datetime.datetime.now(timezone)
+    
+    # Format the date and time
+    formatted_time = current_time.strftime("%a %d/%m/%y %H:%M %Z")
+    
+    # Return the formatted message string
+    return f"At {formatted_time} {discord_id} ({username}) said: {content.strip()}"
